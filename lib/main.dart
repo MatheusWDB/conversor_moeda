9ddd2d75767921fdab0de95cf1db045e29e0bd32
MatheusWDB <matheuswdb1703@gmail.com>
2024-12-26 11:49:21 -0300

    Construindo com FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:conversor_moedas/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
-import 'dart:convert';
-
-const req = 'https://api.hgbrasil.com/finance?key=faf4a310';
 
 void main() async {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(const MyApp());
 }
 
-Future<Map> getData() async {
-  http.Response res = await http.get(Uri.parse(req));
-  return json.decode(res.body);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          )),
+      home: const HomePage(),
+    );
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const req = 'https://api.hgbrasil.com/finance?key=faf4a310';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,8 +23,47 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.amber,
           centerTitle: true,
         ),
-        body: FutureBuilder(),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    'Carregando...!',
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Erro ao carregar!',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }
+                return Container(
+                  color: Colors.green,
+                );
+            }
+          },
+        ),
       ),
     );
   }
+}
+
+Future<Map> getData() async {
+  http.Response res = await http.get(Uri.parse(req));
+  return json.decode(res.body);
 }
